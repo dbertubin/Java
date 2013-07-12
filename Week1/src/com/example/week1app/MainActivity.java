27e48fdcc996d470910b0223574415d50c2673dc
Derek Bertubin <dbertubin@fullsail.edu>
2013-07-12 02:25:37 -0400

    Updated Comments and fixed float bug

--- Week1/src/com/example/week1app/MainActivity.java
+++ Week1/src/com/example/week1app/MainActivity.java
@@ -30,13 +30,13 @@ LinearLayout - check
 
 package com.example.week1app;
 
-import java.lang.reflect.Array;
+//import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.text.Editable;
-import android.util.Log;
+//import android.text.Editable;
+//import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -54,41 +54,52 @@ public class MainActivity extends Activity {
 	LinearLayout.LayoutParams lp;
 	ArrayList<Integer> grades;
 	int gradeValue; 
-	int sum;
+	float sum;
 	float total;
 	
 	String[]gradeArray = {"A","B","C","D","F"};
 	String fieldText;
 
+    /* (non-Javadoc)
+     * @see android.app.Activity#onCreate(android.os.Bundle)
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        
-        
-        
-       // This will create the headline in the view and will display "Calculate your GPA"  
+       
+        /*
+         * This will create the headline in the view and will display "Calculate your GPA"
+         */
         ll = new LinearLayout(this);
         ll.setOrientation(LinearLayout.VERTICAL);
         lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-//        ll.setLayoutParams(lp);
+
         
         TextView textView = new TextView(this);
         textView.setText("Calculate your GPA"); 
-        
-
         
         final EditText et = new EditText(this);
         et.setHint("Enter Letter Grade");
         ll.addView(et);
         
+        /*
+         * Add Grade Button 
+         */
         Button b = new Button(this);
         b.setText("Add Grade"); 
        
+        /*
+         * Instantiate the ArrayList and additional vars to store data
+         */
         grades = new ArrayList<Integer>();
         fieldText = new String();
         fieldText = et.getText().toString();
         
+        /*
+         * Button on click checks takes the text from the EditText Field and converts it to a string then checks the length to
+         * see if it has one character or not
+         * If length = 0 then it will  check for the string value and then add a corresponding int value to the ArrayList 
+         */
         b.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
@@ -146,6 +157,9 @@ public class MainActivity extends Activity {
 			}
 		});
 	
+        /*
+         *  Calc Button adds all of the values in the array and divides them by the number of values in the array
+         */
 
         Button calc = new Button(this);
         calc.setText("Calculate GPA");
@@ -162,11 +176,14 @@ public class MainActivity extends Activity {
 				
 				System.out.println(total);	
 				}
-				total =0;
-				sum = 0;
+				
 			}
 		});
         
+        
+        /*
+         * Clear button resets the app by emptying the ArrayList and setting anyother values to 0
+         */
         Button clear = new Button(this);
         clear.setText("Clear");
         clear.setOnClickListener(new View.OnClickListener() {
@@ -199,6 +216,9 @@ public class MainActivity extends Activity {
 
 
    
+    /* (non-Javadoc)
+     * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
