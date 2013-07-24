f14fc0c3d7f4b6892207ead8f948aa2ca6ee97e7
Derek Bertubin <dbertubin@fullsail.edu>
2013-07-24 01:54:04 -0400

    Added all the classed needed for project
    at least for now...

--- Week3/ChuckNorris/src/com/petrockz/chucknorris/lib/NetworkConnection.java
+++ Week3/ChuckNorris/src/com/petrockz/chucknorris/lib/NetworkConnection.java
@@ -3,5 +3,6 @@ package com.petrockz.chucknorris.lib;
 public class NetworkConnection {
 	
 	// Create Connection Handler and Api Connection 
+	// since this is a common function this most likely will be the JAR file. 
 
 }
---
+++ Week3/ChuckNorris/src/com/petrockz/chucknorris/lib/ReadWrite.java
@@ -0,0 +1,5 @@
+package com.petrockz.chucknorris.lib;
+
+public class ReadWrite {
+	// this might also be a the JAR 
+}
