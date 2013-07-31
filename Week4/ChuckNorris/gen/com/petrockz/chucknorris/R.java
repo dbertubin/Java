7c476eb5cb3ad3a05cb66c7609c291019006bbcb
Derek Bertubin <dbertubin@fullsail.edu>
2013-07-31 18:25:22 -0400

    implemented basic styles

--- Week4/ChuckNorris/gen/com/petrockz/chucknorris/R.java
+++ Week4/ChuckNorris/gen/com/petrockz/chucknorris/R.java
@@ -74,5 +74,7 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int JokeLabel=0x7f060002;
+        public static final int JokeText=0x7f060003;
     }
 }
